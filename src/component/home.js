18a138aa296db502159185781d41c7b3ac845782
Sujit Singh <33032751+baby-developer@users.noreply.github.com>
2020-03-13 17:11:35 +0530

    added some functional comp

--- src/component/home.js
+++ src/component/home.js
@@ -1,53 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container,Row,Col,Card,Button,Modal } from 'react-bootstrap';
+import { Container,Row,Col,Card,Table } from 'react-bootstrap';
+import StartShopping from './startShopping'
 
-const size={
-    fontSize: '20px'
-};
-function StartScan(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Scan the QR code to start your shopping 
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="my-5">
-          QR CODE SCANNER
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-}
-
-function Home(){
-    const [modalShow, setModalShow] = React.useState(false);
-    
+function Home(){    
     return (  
         <Container>
-            <Row className="mt-5">
-                <Col>
-                    <Card className="text-center mt-5">
-                        <Card.Body>
-                            <Card.Title style={size}>Welcome to KwikCart</Card.Title>
-                            <Button className="mt-5" variant="primary" onClick={() => setModalShow(true)}>Start Shoping</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+            <StartShopping/>
             <Row>
                 <Col>
-                    <Card>
+                    <Card className="my-5">
                         <Card.Body>
-                           
+                            <Card.Text><b>My Cart</b></Card.Text>
+                            <Table responsive>
+                                <thead>
+                                    <tr>
+                                        <th>Products</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Apple</td>
+                                        <td>2</td>
+                                        <td>30</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
                         </Card.Body>
                         <Card.Footer>
                             <span className="mr-auto"><b>Total: $999</b></span>
@@ -55,10 +35,6 @@ function Home(){
                     </Card>
                 </Col>
             </Row>
-            <StartScan
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
         </Container>
     );
 }
---
+++ src/component/startShopping.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row,Col,Card,Button,Modal} from 'react-bootstrap';
+
+function StartScan(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Scan the QR code to start your shopping 
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="my-5">
+          QR CODE SCANNER
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
+
+function StartShopping() {
+    const [modalShow, setModalShow] = React.useState(false);
+    return ( 
+        <Row className="mt-5">
+            <Col>
+                <Card className="text-center mt-5">
+                    <Card.Body>
+                        <Card.Title>Welcome to KwikCart</Card.Title>
+                        <Button className="mt-5" variant="primary" onClick={() => setModalShow(true)}>Start Shoping</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <StartScan
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </Row> 
+    );
+}
+ 
+export default StartShopping;
